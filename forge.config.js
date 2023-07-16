85720ed9a4c7559e71af34cabeafca2d59b2bed9
Mike Pruett <mikepruett3@gmail.com>
2023-07-16 13:26:32 -0500

    Paired the Action down to only build the linux ver

--- forge.config.js
+++ forge.config.js
@@ -5,32 +5,32 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        iconUrl: __dirname + '/images/Reddit.ico',
-        setupIcon: './images/Reddit.ico'
-      },
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
+    //{
+    //  name: '@electron-forge/maker-squirrel',
+    //  config: {
+    //    iconUrl: __dirname + '/images/Reddit.ico',
+    //    setupIcon: './images/Reddit.ico'
+    //  },
+    //},
+    //{
+    //  name: '@electron-forge/maker-zip',
+    //  platforms: ['darwin'],
+    //},
     {
       name: '@electron-forge/maker-deb',
-      options: {
+      config: {
         name: 'reddit-desktop',
         productName: 'reddit-desktop'
       }
     },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {
-        options: {
-          name: 'reddit-desktop',
-          productName: 'reddit-desktop'
-        }
-      }
-    }
+    //{
+    //  name: '@electron-forge/maker-rpm',
+    //  config: {
+    //    options: {
+    //      name: 'reddit-desktop',
+    //      productName: 'reddit-desktop'
+    //    }
+    //  }
+    //}
   ]
 }
